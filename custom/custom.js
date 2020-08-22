5efc34aeb077fccc14cc5b345a56ff7379a5e36d
MICHAEL FRANIATTE <michael.franiatte@gmail.com>
2020-08-22 09:02:41 +0200

    Tried progressive scroll to anchor links

--- custom/custom.js
+++ custom/custom.js
@@ -3,4 +3,19 @@ $(function() {
   $('#sidebarCollapse').on('click', function() {
     $('#sidebar, #content').toggleClass('active');
   });
+    /**
+    * Smooth scrolling to page anchor on click
+    **/
+    $("a[href*='#']:not([href='#'])").click(function() {
+        if (
+            location.hostname == this.hostname
+            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+        ) {
+            var anchor = $(this.hash);
+            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+            if ( anchor.length ) {
+                $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
+            }
+        }
+    });
 });
