1d5b442978cc54fd886d65cc2a61134cdf076fea
MICHAEL FRANIATTE <michael.franiatte@gmail.com>
2020-08-23 10:38:26 +0200

    Update custom.js

--- custom/custom.js
+++ custom/custom.js
@@ -21,6 +21,7 @@ $(function() {
             anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
             if (anchor.length) {
                 $("html, body").animate({ scrollTop: anchor.offset().top }, 1500);
+	    	valsection = anchor;
             }
         }
     });
