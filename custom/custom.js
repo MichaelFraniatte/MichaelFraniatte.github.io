12a3a40a085c272acf5bd158bbaf2f565cf78adb
MICHAEL FRANIATTE <michael.franiatte@gmail.com>
2020-08-23 06:04:00 +0200

    Added scrollspy

--- custom/custom.js
+++ custom/custom.js
@@ -1,16 +1,16 @@
 $(function() {
-  // Sidebar toggle behavior
-  $('#sidebarCollapse').on('click', function() {
-    $('#sidebar, #content').toggleClass('active');
-  });
+
+    // Sidebar toggle behavior
+    $('#sidebarCollapse').on('click', function() {
+        $('#sidebar, #content').toggleClass('active');
+    });
+
     /**
     * Smooth scrolling to page anchor on click
     **/
     $("a[href*='#']:not([href='#'])").click(function() {
-        if (
-            location.hostname == this.hostname
-            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
-        ) {
+        if (location.hostname == this.hostname && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")) 
+	{
             var anchor = $(this.hash);
             anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
             if ( anchor.length ) {
@@ -18,4 +18,23 @@ $(function() {
             }
         }
     });
+
+    //ScrollSpy
+    jQuery(window).scroll(checkActiveSection);
+    jQuery(document).ready(checkActiveSection);
+
 });
+
+function checkActiveSection()
+{
+    var fromTop = jQuery(window).scrollTop() ;
+    jQuery('#sections .section').each(function(){
+        var sectionOffset = jQuery(this).offset() ;
+        if ( sectionOffset.top <= fromTop )
+        {
+            jQuery('#navbar li').removeClass('active') ;
+            jQuery('#navbar li[data-id="'+jQuery(this).data('id')+'"]').addClass('active') ;
+            
+        }
+    }) ;
+}
