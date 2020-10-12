8b32e0ea151e817e0e79471300dc702ef89ba939
MICHAEL FRANIATTE <michael.franiatte@gmail.com>
2020-10-12 04:03:21 +0200

    Removed the typewriter for added mine

--- custom/custom.js
+++ custom/custom.js
@@ -64,56 +64,51 @@ function enableActiveSection() {
 }
 
 //Type Writer
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
+var i = 0;
+var speed = 100;
+var j = 0;
+var txt = 'Creative';
+var newtxt = '';
+var txtarray = ["Creative", "Imaginative", "Worker", "Developer"];
+var next = 1;
 
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } 
-    else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
+function typeWriter() {
+    var htmlString = '';
+    if (i < txt.length & j == 0) {
+        document.getElementById("typewrite").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
     }
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-    if (this.isDeleting) { 
-        delta /= 2; 
+    if (i < txt.length & j == 1) {
+        document.getElementById("typewrite").innerHTML = txt;
+        i++;
+        setTimeout(typeWriter, speed);
     }
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } 
-    else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
+    if (i < txt.length & j == 2) {
+        newtxt = removeByIndex(newtxt, txt.length - i - 1)
+        document.getElementById("typewrite").innerHTML = newtxt;
+        i++;
+        setTimeout(typeWriter, speed);
     }
-    setTimeout(function() {
-        that.tick();
-    }, delta);
-};
-
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-            new TxtType(elements[i], JSON.parse(toRotate), period);
+    if (i >= txt.length) {
+        i = 0;
+        j++;
+        newtxt = txt;
+    }
+    if (j >= 3) {
+        j = 0;
+        txt = txtarray[next];
+        next++;
+        if (next >= 4) {
+            next = 0;
         }
     }
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
-};
+}
+
+function removeByIndex(str,index) {
+    return str.slice(0, index) + str.slice(index + 1);
+}
+
+$(function() {
+    typeWriter();
+});
